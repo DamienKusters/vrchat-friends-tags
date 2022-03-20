4339b7410f3766dc07e9f4e267647daa12e48a73
Damien Kusters <damien.kusters@ziggo.nl>
2022-03-20 13:27:21 +0100

    Improve UX

--- browserAction/script.js
+++ browserAction/script.js
@@ -3,7 +3,7 @@ var fullUserId = null;
 browser.runtime.sendMessage("get").then(res => {
     document.getElementById("lbl_userName").innerText = res.userName;
     fullUserId = res.userId;
-    document.getElementById("lbl_userId").innerText = fullUserId.slice(0,8) + "...";
+    document.getElementById("lbl_userId").innerText = fullUserId;
     if(res.tags.length > 0)
         res.tags.forEach(tag => {
             document.getElementById("content_body").innerHTML += "<div class='tag'>"+tag+"<div class='btn_del'></div></div>";
@@ -12,16 +12,13 @@ browser.runtime.sendMessage("get").then(res => {
 
 document.getElementById("btn_tag").addEventListener("click", () => {
     const text = document.getElementById("tbx_tag").value;
+    if(text == "")
+        return;
     document.getElementById("content_body").innerHTML += "<div class='tag'>"+text+"<div class='btn_del'></div></div>";
     browser.runtime.sendMessage({"tag": text});
+    document.getElementById("tbx_tag").value = "";
 });
 
-document.getElementById("lbl_userId").addEventListener("mouseenter", () => {
-    if(fullUserId != null)
-        document.getElementById("lbl_userId").innerText = fullUserId;
-});
-
-document.getElementById("lbl_userId").addEventListener("mouseleave", () => {
-    if(fullUserId != null)
-        document.getElementById("lbl_userId").innerText = fullUserId.slice(0,8) + "...";
+document.getElementById("btn_del").addEventListener("click", () => {
+    //TODO
 });
